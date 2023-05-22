3b6b6c18c49ee880acad935f9ccc47a8d5a7d4ef
khlvamir <khlvamir@gmail.com>
2023-05-22 11:39:54 +0200

    Added socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,38 @@
-import "reflect-metadata";
-import express, { Request, Response } from "express";
 import { AppDataSource } from "./db/data-source.js";
+import { Server, Socket } from "socket.io";
+import "reflect-metadata";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import http from "http";
+const app = express();
+
+app.use(cors);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: "*",
+    allowedHeaders: ["*/*"],
+  },
+});
 
 AppDataSource.initialize().then(() => {
   console.log("📚 db initialized");
 });
 
-const app = express();
+io.on("connection", (socket: Socket) => {
+  console.log("User connected to socket");
+
+  setInterval(() => {
+    socket.emit("update", "data base update");
+  }, 1000);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("works");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Backend started 💀💀");
 });
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  ngOnInit(): void {
+    const socket = io('http://localhost:3000');
+    socket.on('update', (res) => {
+      console.log(res);
+    });
+  }
 }
