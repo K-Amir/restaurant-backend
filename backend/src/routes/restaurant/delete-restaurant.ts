1039f92ca6275d6d3d0a9a5d0f61ef886dc7b60d
khlvamir <khlvamir@gmail.com>
2023-05-28 00:29:08 +0200

    Update delete action

--- backend/src/routes/restaurant/delete-restaurant.ts
+++ backend/src/routes/restaurant/delete-restaurant.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../../db/data-source.js";
 import { Restaurant } from "../../db/entity/restaurant.js";
+import { Error } from "../../errors/error.response.js";
 
 const restaurantRepo = AppDataSource.getRepository(Restaurant);
 
 const deleteRestaurantById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const restaurantToDelete = await restaurantRepo.delete({
+  const { affected } = await restaurantRepo.delete({
     id: Number(id),
   });
 
+  // When restaurant not found send error
+  if (affected <= 0) {
+    res.status(404).send(Error.response(403, "Not found", "Entity not found"));
+    return;
+  }
+
   res.send({
     status: "Deleted successfully",
-    restaurant: restaurantToDelete,
   });
 };
 
